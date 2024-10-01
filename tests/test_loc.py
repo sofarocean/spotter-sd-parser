8698345219f03ebe0e1d32c6ea7d5bd80994d173
Tim Johnson <2293335+tcj@users.noreply.github.com>
2024-09-30 17:32:22 -0700

    add fix for V3 LOC.csv parsing & unit test coverage

--- tests/test_loc.py
+++ tests/test_loc.py
@@ -6,6 +6,7 @@ test location file parsing
 
 import os
 import shutil
+import sys
 import tempfile
 import time
 import unittest
@@ -14,14 +15,23 @@ from math import ceil
 from sd_file_parser import parseLocationFiles
 
 class LocationParsingTest(unittest.TestCase):
-
     def testBasicParserRun(self):
-        if os.path.exists("displacement.csv"):
-            os.remove("displacement.csv")
         parseLocationFiles( inputFileName = self.inputfn, kind='LOC', outputFileName=self.outputfn )
         self.assertTrue( os.path.exists( self.outputfn ) ) 
         self.assertFalse( os.path.exists( 'displacement.csv' ) )
-    
+
+    def testOutputLocationIsValidFloat(self):
+        # test against bug where V3 LOC files are misinterpreted as not being floats
+        parseLocationFiles( inputFileName = r'example_data/2024-09-23/0002_LOC.csv', kind='LOC', outputFileName=self.outputfn )
+        self.assertTrue( os.path.exists( self.outputfn ) ) 
+        # print(f"wrote file to {self.outputpath}...?", file=sys.stderr)
+        with open(self.outputfn, 'r') as csvf:
+            # bad: 2024,9,23,14,54,24,0,  37.00000000,-122.00000000
+            # skip header
+            _ = csvf.readline()
+            firstline = csvf.readline()
+        self.assertFalse(firstline.rstrip().endswith(',  37.00000000,-122.00000000'))
+
     def testNoOutputPathParserRun(self):
         """
         show that parseLocationFiles() can't take an output path argument
@@ -36,9 +46,10 @@ class LocationParsingTest(unittest.TestCase):
         """
         prepare for running the parser
         """
+        if os.path.exists("displacement.csv"):
+            os.remove("displacement.csv")
         self.inputfn = 'example_data/2021-01-15/0235_LOC.CSV'        
-        # self.outputfn = f"spctest_{ceil(time.time()):x}.csv"
-        self.outputfn = 'location.csv'
+        self.outputfn = f"location_{ceil(time.time()):x}.csv"
         self.outputpath = tempfile.mkdtemp()
         if not self.outputpath:
             raise ValueError('problem creating temporary output directory')
